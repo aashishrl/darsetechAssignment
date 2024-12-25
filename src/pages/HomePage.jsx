e553da8b31841eb810eedc2f417c9f48c817852a
Aashish Ramtel <aaseshrl@gmail.com>
2024-12-25 12:35:58 +0545

    Project File Setup: React and Tailwindcss

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage (){
+    return (
+        <div className='text-4xl text-green-600'>
+            what is up what is up
+        </div>
+    )
+}
+
+export default HomePage;
