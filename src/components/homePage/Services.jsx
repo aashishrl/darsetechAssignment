e6a05fad4b4a6c20f386162a307f73b40c891d4c
Aashish Ramtel <aaseshrl@gmail.com>
2024-12-29 18:29:06 +0545

    fix: resolve issue with services section image not displaying

--- src/components/homePage/Services.jsx
+++ src/components/homePage/Services.jsx
@@ -13,7 +13,7 @@ function Services() {
               <h4 className="font-bold text-lg pb-3 flex items-center gap-2">
                 <img
                   className="h-5"
-                  src="\assests\check.png"
+                  src="\assests\services\check.png"
                   alt="check-icon-img"
                 />{" "}
                 Personal Training
@@ -25,7 +25,7 @@ function Services() {
               <h4 className="font-bold text-lg pb-3 flex items-center gap-2">
                 <img
                   className="h-5"
-                  src="\assests\check.png"
+                  src="\assests\services\check.png"
                   alt="check-icon-img"
                 />{" "}
                 Expert Trainer
